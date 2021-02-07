83800a3492cb7befaf70012b45a6cdcc458075d9
Bruno Cayres Messias <brunocmessias@hotmail.com>
2021-02-07 20:25:07 -0300

    solving adder

--- Course FPGA/Adder_4bits/Add4.vhd
+++ Course FPGA/Adder_4bits/Add4.vhd
@@ -7,24 +7,24 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 
-
 -- Entity
-entity Add4 is port(
-	Data1,Data2		: in std_logic_vector(3 downto 0);
-	Cin				: in std_logic;
-	Cout			  : out std_logic;
-	Sum			  : out std_logic_vector(3 downto 0));
+entity Add4 is
+    port(
+        Data1, Data2 : in  std_logic_vector(3 downto 0);
+        Cin          : in  std_logic;
+        Cout         : out std_logic;
+        Sum          : out std_logic_vector(3 downto 0));
 end entity;
 
 -- Architecture
 architecture rtl of Add4 is
 
-	signal Out5Bit : std_logic_vector(4 downto 0);
+    signal Out5Bit : std_logic_vector(4 downto 0);
 
 begin
 
-	Out5Bit <= ('0' & Data1) + ('0' & Data2) + Cin;
-	Sum 	  <= Out5Bit(3 downto 0); --4 bits
-	Cout	  <= Out5Bit(4); 		     --5th bit
-	
+    Out5Bit <= ('0' & Data1) - ('0' & Data2) + Cin;
+    Sum     <= Out5Bit(3 downto 0);     --4 bits
+    Cout    <= Out5Bit(4);              --5th bit
+
 end architecture;
